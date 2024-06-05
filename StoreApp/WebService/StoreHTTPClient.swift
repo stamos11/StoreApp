66ddea7eb36d29a44af11a6c85e4ace6fbf92249
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-06-05 23:57:33 +0300

    Implementing Generic network layer in StoreHttpClient

--- StoreApp/WebService/StoreHTTPClient.swift
+++ StoreApp/WebService/StoreHTTPClient.swift
@@ -36,6 +36,42 @@ struct Resource<T: Codable> {
 }
 class StoreHTTPClient {
     
+    func load<T: Codable>(_ resource: Resource<T>) async throws -> T {
+        
+        var request = URLRequest(url: resource.url)
+        
+        switch resource.method {
+        case .get(let queryItems):
+            var components = URLComponents(url: resource.url, resolvingAgainstBaseURL: true)
+            components?.queryItems = queryItems
+            guard let url = components?.url else {
+                throw NetworkError.InvalidUrl
+            }
+            request = URLRequest(url: url)
+        case .post(let data):
+            request.httpBody = data
+        default:
+            break
+        }
+        request.allHTTPHeaderFields = resource.headers
+        request.httpMethod = resource.method.name
+        
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        
+        let session = URLSession(configuration: configuration)
+        
+        let (data, response) = try await session.data(for: request)
+        guard let _ = response as? HTTPURLResponse
+        else {
+            throw NetworkError.InvalidServerResponse
+        }
+        guard let result = try? JSONDecoder().decode(T.self, from: data) else {
+            throw NetworkError.DecodingError
+        }
+        return result
+    }
+    
     func getAllCategories() async throws -> [Category] {
         
         
